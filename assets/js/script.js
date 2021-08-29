398fda4181479c3e45942a3bd37a270973af960e
stefan hall <stefhall@comcast.net>
2021-08-29 13:18:08 -0700

    Testing out initial framework... buttons aren't quite working. Need to adjust getElementById

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var startBtnEl = document.querySelector('#start');
+var timerEl = document.getElementById("countdown");
+var quizEl = document.getElementById("quiz");
+var questionsEl = document.getElementById("questions");
+var resultsEl = document.getElementById("result");
+var currentScore = document.getElementById('current-score');
+
+var submitScoreBtn = document.getElementById("submitScore");
+var submitBtnEl = document.querySelector('#submit-initials');
+
+var A = document.getElementById("1");
+var B = document.getElementById("2");
+var C = document.getElementById("3");
+var D = document.getElementById("4");
