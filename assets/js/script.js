293055a6cb28cb3e129687e8c6e6933999aceac8
stefan hall <stefhall@comcast.net>
2021-08-29 13:40:17 -0700

    Trying to get display questions to show up. Currently, only the answer and the buttons appear when I click start Quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,29 @@ var A = document.getElementById("1");
 var B = document.getElementById("2");
 var C = document.getElementById("3");
 var D = document.getElementById("4");
+
+// Questions!
+var questions = [{
+    question: "Why do JavaScript and Java have similar names?",
+    answer1: "JavaScript is a stripped-down version of Java",
+    answer2: "JavaScript's syntax is loosely based on Java's",
+    answer3: "They both originated on ther island of Java",
+    answer4: "None of The Above",
+    correctAnswer: "b"},  
+
+];
+
+var currentQuestionIndex = 0;
+
+var displayQuestion = function () {
+    var currentQuestion = questions[currentQuestionIndex];
+    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    A.innerHTML = currentQuestion.answer1;
+};
+
+function startQuiz(){
+
+    displayQuestion();
+}
+
+startBtnEl.addEventListener("click",startQuiz);
